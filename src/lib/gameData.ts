9625185ab45efa5e30fa84819f12e540371d8675
Nikhil Gupta <nkantgupta@gmail.com>
2025-07-19 22:39:10 -0400

    new choices

--- src/lib/gameData.ts
+++ src/lib/gameData.ts
@@ -1,10 +1,10 @@
 export const mommy = 'Mommy';
 export const daddy = 'Daddy';
 export const maleCelebrities = [
-    'Owen Wilson', 'Benedict Cumberbatch', 'Rupert Grint', 'Timothee Chalamet', 'Daddy'
+    'Owen Wilson', 'Benedict Cumberbatch', 'Rupert Grint', 'Timothee Chalamet', 'Daniel Radcliffe',  'Robert Pattinson' 
 ];
 export const femaleCelebrities = [
-    'Priyanka Chopra', 'Halle Berry', 'Zendaya', 'Mindy Kaling'
+    'Priyanka Chopra', 'Halle Berry', 'Zendaya', 'Mindy Kaling', 'Zoe Saldana',  'Kerry Washington',  'Mommy'
 ];
 
 export const gameData = [
@@ -48,11 +48,76 @@ export const gameData = [
         babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_timothee.jpeg',
         correctParents: { mom: mommy, dad: 'Timothee Chalamet' }
     },
+    // {
+    //     babyId: 'baby10',
+    //     babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_elijah.jpeg',
+    //     correctParents: { mom: mommy, dad: 'Elijah Wood' }
+    // },
     {
-        babyId: 'baby9',
+        babyId: 'baby11',
+        babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_daniel.jpeg',
+        correctParents: { mom: mommy, dad: 'Daniel Radcliffe' }
+    },
+    // {
+    //     babyId: 'baby12',
+    //     babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_orlando.jpeg',
+    //     correctParents: { mom: mommy, dad: 'Orlando Bloom' }
+    // },
+    {
+        babyId: 'baby13',
+        babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_robert.jpeg',
+        correctParents: { mom: mommy, dad: 'Robert Pattinson' }
+    },
+    // {
+    //     babyId: 'baby15',
+    //     babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_chriswalken.jpeg',
+    //     correctParents: { mom: mommy, dad: 'Young Christopher Walken' }
+    // },
+    // {
+    //     babyId: 'baby16',
+    //     babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_jacknich.jpeg',
+    //     correctParents: { mom: mommy, dad: 'Young Jack Nicholson' }
+    // },
+    // {
+    //     babyId: 'baby17',
+    //     babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_harrison.jpeg',
+    //     correctParents: { mom: mommy, dad: 'Young Harrison Ford' }
+    // },
+    // {
+    //     babyId: 'baby18',
+    //     babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/mom_tom.jpeg',
+    //     correctParents: { mom: mommy, dad: 'Young Tom Hanks' }
+    // },
+    {
+        babyId: 'baby19',
+        babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/dad_zoe.jpeg',
+        correctParents: { mom: daddy, dad: 'Zoe Saldana' }
+    },
+    // {
+    //     babyId: 'baby20',
+    //     babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/dad_rihanna.jpeg',
+    //     correctParents: { mom: daddy, dad: 'Rihanna' }
+    // },
+    // {
+    //     babyId: 'baby21',
+    //     babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/dad_beyonce.jpeg',
+    //     correctParents: { mom: daddy, dad: 'Beyonce' }
+    // },
+    {
+        babyId: 'baby22',
+        babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/dad_kerry.jpeg',
+        correctParents: { mom: daddy, dad: 'Kerry Washington' }
+    },
+    {
+        babyId: 'baby23',
+        babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/dad_freida.jpeg',
+        correctParents: { mom: daddy, dad: 'Freida Pinto' }
+    },
+    {
+        babyId: 'correctBaby',
         babyImage: 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_baby/dad_mom.jpeg',
         correctParents: { mom: mommy, dad: daddy }
-    }
+    },
 ];
 
 export const getCoupleString = (mom: string, dad: string) => {
@@ -70,12 +135,25 @@ export const getCoupleFromString = (coupleString: string) => {
 export const celebrityImages = {
     'Mommy': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_main.jpeg',
     'Daddy': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_main.jpg',
-    'Owen Wilson': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/owen.png',
-    'Benedict Cumberbatch': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/benedict.png',
-    'Rupert Grint': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/rupert.png',
-    'Priyanka Chopra': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/priyanka.jpg',
-    'Halle Berry': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/halle.png',
-    'Mindy Kaling': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/mindy.jpg',
-    'Zendaya': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/zendaya.jpeg',
-    'Timothee Chalamet': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/timothee.png'
+    'Owen Wilson': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_02_owen.png',
+    'Benedict Cumberbatch': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_01_benedict.png',
+    'Rupert Grint': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_03_rupert.png',
+    'Priyanka Chopra': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_03_priyanka.jpg',
+    'Halle Berry': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_01_halle.png',
+    'Mindy Kaling': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_02_mindy.jpg',
+    'Zendaya': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_04_zendaya.jpeg',
+    'Timothee Chalamet': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_04_timothee.png',
+    'Elijah Wood': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_06_elijah.jpg',
+    'Daniel Radcliffe': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_07_daniel_radcliffe.jpeg',
+    'Orlando Bloom': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_08_orlando_bloom.jpeg',
+    'Robert Pattinson': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_09_robert_pattinson.jpeg',
+    'Young Christopher Walken': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_11_young_christopher_walken.jpeg',
+    'Young Jack Nicholson': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_12_young_jack_nicholson.jpeg',
+    'Young Harrison Ford': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_05_young_harrison_ford.jpeg',
+    'Young Tom Hanks': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/mom_10_young_tom_hanks.jpeg',
+    'Zoe Saldana': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_05_zoe_saldana.jpeg',
+    'Rihanna': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_06_rihanna.jpeg',
+    'Beyonce': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_07_beyonce.png',
+    'Kerry Washington': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_08_kerry_washington.jpg',
+    'Freida Pinto': 'https://raw.githubusercontent.com/madelinengo/baby-shower-game/refs/heads/main/images/pics_parents_for_app/dad_09_freida_pinto.jpeg'
 };
